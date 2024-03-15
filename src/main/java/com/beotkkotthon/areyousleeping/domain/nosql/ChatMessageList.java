b9380439a483792c4ee2b57c5749b31e152f384f
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-16 03:20:50 +0900

    feat: ChatMessage, ChatMessageList 도메인 구현

--- src/main/java/com/beotkkotthon/areyousleeping/domain/nosql/ChatMessageList.java
+++ src/main/java/com/beotkkotthon/areyousleeping/domain/nosql/ChatMessageList.java
@@ -1,2 +1,15 @@
-package com.beotkkotthon.areyousleeping.domain.nosql;public class ChatMessageList {
+package com.beotkkotthon.areyousleeping.domain.nosql;
+
+import jakarta.persistence.Id;
+import lombok.Getter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Getter
+@Document(collection = "chats")
+public class ChatMessageList {
+    @Id
+    private String id; // 팀 아이디
+    private List<ChatMessage> messages;
 }
