4bff3b83fe8cab939bcc5edd25ea789bb270a1ad
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-16 03:23:07 +0900

    feat: ChatMessageService 구현

--- src/main/java/com/beotkkotthon/areyousleeping/repository/ChatMessageListRepository.java
+++ src/main/java/com/beotkkotthon/areyousleeping/repository/ChatMessageListRepository.java
@@ -1,10 +1,9 @@
 package com.beotkkotthon.areyousleeping.repository;
 
-import com.beotkkotthon.areyousleeping.domain.nosql.ChatMessage;
 import com.beotkkotthon.areyousleeping.domain.nosql.ChatMessageList;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatMessageRepository extends MongoRepository<ChatMessageList, String> {
+public interface ChatMessageListRepository extends MongoRepository<ChatMessageList, String> {
 }
