066967378bbdf243d70796eeec4566595f5f1b1b
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-16 03:18:51 +0900

    feat: add WebSocketConfig

--- src/main/java/com/beotkkotthon/areyousleeping/config/WebSocketConfig.java
+++ src/main/java/com/beotkkotthon/areyousleeping/config/WebSocketConfig.java
@@ -1,2 +1,23 @@
-package com.beotkkotthon.areyousleeping.config;public class WebSocketConfig {
+package com.beotkkotthon.areyousleeping.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-connection").setAllowedOriginPatterns("*").withSockJS(); // 첫 번째 핸드쉐이킹
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/subscribe");  //메세지 브로커가 메시지를 뿌릴때
+        registry.setApplicationDestinationPrefixes("/app"); //클라에서 메세지 브로커한테 메시지를 보낼때
+    }
 }
