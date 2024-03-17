8dbf9244775e3d6b5334ac7a1fc8a3b5d64a295b
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-18 02:13:57 +0900

    fix: 임시 UserTeamRepository 메소드 추가

--- src/main/java/com/beotkkotthon/areyousleeping/repository/UserTeamRepository.java
+++ src/main/java/com/beotkkotthon/areyousleeping/repository/UserTeamRepository.java
@@ -4,6 +4,10 @@ import com.beotkkotthon.areyousleeping.domain.UserTeam;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
+    UserTeam findByUserIdAndTeamId(Long userId, Long teamId);
+    List<UserTeam> findAllByTeamId(Long teamId);
 }
