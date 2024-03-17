8ccd77927b407f688b7fcd8870c741793715f102
leeeunda <daun0219@gmail.com>
2024-03-17 23:32:46 +0900

    feat: 팀 생성 API

--- src/main/java/com/beotkkotthon/areyousleeping/controller/TeamController.java
+++ src/main/java/com/beotkkotthon/areyousleeping/controller/TeamController.java
@@ -1,2 +1,37 @@
-package com.beotkkotthon.areyousleeping.controller;public class TeamController {
+package com.beotkkotthon.areyousleeping.controller;
+
+import com.beotkkotthon.areyousleeping.annotation.UserId;
+import com.beotkkotthon.areyousleeping.domain.Team;
+import com.beotkkotthon.areyousleeping.dto.global.ResponseDto;
+import com.beotkkotthon.areyousleeping.dto.request.TeamSaveDto;
+import com.beotkkotthon.areyousleeping.dto.response.TeamResponseDto;
+import com.beotkkotthon.areyousleeping.service.TeamService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class TeamController {
+
+    private final TeamService teamService;
+
+    // 팀 생성
+    @PostMapping("/team")
+    @Operation(summary = "팀 생성", description = "팀 생성 API")
+    public ResponseDto<?> createTeam(@UserId Long userId, @RequestBody @Valid TeamSaveDto teamSaveDto){
+
+        Team createdTeam=teamService.createTeam(userId, teamSaveDto);
+        TeamResponseDto teamResponseDto = new TeamResponseDto(createdTeam);
+
+        return ResponseDto.ok(teamResponseDto);
+    }
+
+    // 유저를 팀에 가입
+//    @PostMapping("/{teamId}")
+//    public ResponseDto<String> addUserToTeam(@PathVariable Long userId, @PathVariable Long teamId){
+//        TeamService.addUserToTeam(userId, teamId);
+//    }
 }
