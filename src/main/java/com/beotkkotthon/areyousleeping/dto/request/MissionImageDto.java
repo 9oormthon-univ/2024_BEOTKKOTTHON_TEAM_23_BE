d1fafe7fd076d8a1ec80cd99fbdbac402b0a384c
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-18 02:08:19 +0900

    fix: MissionImageDto 간소화

--- src/main/java/com/beotkkotthon/areyousleeping/dto/request/MissionImageDto.java
+++ src/main/java/com/beotkkotthon/areyousleeping/dto/request/MissionImageDto.java
@@ -1,30 +1,10 @@
 package com.beotkkotthon.areyousleeping.dto.request;
 
-import com.beotkkotthon.areyousleeping.domain.Mission;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
-
-import java.time.LocalDateTime;
 @Builder
-public record MissionDto(
-        @JsonProperty("user_team_id")
-        Long userTeamId,
-        @JsonProperty("issued_at")
-        LocalDateTime issuedAt,
+public record MissionImageDto(
         @JsonProperty("result")
-        Boolean result,
-        @JsonProperty("result_image_url")
-        String resultImageUrl,
-        @JsonProperty("result_text")
-        String resultText
+        Boolean result
 ) {
-    public static MissionDto fromEntity(Mission mission) {
-        return MissionDto.builder()
-                .userTeamId(mission.getUserTeam().getId())
-                .issuedAt(mission.getIssuedAt())
-                .result(mission.getResult())
-                .resultImageUrl(mission.getResultImageUrl())
-                .resultText(mission.getResultText())
-                .build();
-    }
 }
