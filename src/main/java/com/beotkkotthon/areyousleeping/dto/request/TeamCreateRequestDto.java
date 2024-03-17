6df950794a68bc4d1ca18b607bff1e0020c668e7
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-18 02:13:33 +0900

    feat: 임시 TeamCreateRequestDto 구현

--- src/main/java/com/beotkkotthon/areyousleeping/dto/request/TeamCreateRequestDto.java
+++ src/main/java/com/beotkkotthon/areyousleeping/dto/request/TeamCreateRequestDto.java
@@ -1,2 +1,22 @@
-package com.beotkkotthon.areyousleeping.dto.request;public class TeamCreateRequestDto {
+package com.beotkkotthon.areyousleeping.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+@Builder
+public record TeamCreateRequestDto(
+        @JsonProperty("title")
+        String title,
+        @JsonProperty("max_num")
+        Integer maxNum,
+        @JsonProperty("target_time")
+        Integer targetTime,
+        @JsonProperty("isSecret")
+        Boolean isSecret,
+        @JsonProperty("password")
+        String password,
+        @JsonProperty("category")
+        String category,
+        @JsonProperty("description")
+        String description
+) {
 }
