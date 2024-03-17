d13cf61b1b6f8216093ffd7fb2882829fc3648b2
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-18 02:08:32 +0900

    feat: MissionTextDto 추가

--- src/main/java/com/beotkkotthon/areyousleeping/dto/request/MissionTextDto.java
+++ src/main/java/com/beotkkotthon/areyousleeping/dto/request/MissionTextDto.java
@@ -1,2 +1,13 @@
-package com.beotkkotthon.areyousleeping.dto.request;public class MissionTextDto {
+package com.beotkkotthon.areyousleeping.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+@Builder
+public record MissionTextDto(
+        @JsonProperty("result")
+        Boolean result,
+        @JsonProperty("result_text")
+        String resultText
+) {
 }
+
