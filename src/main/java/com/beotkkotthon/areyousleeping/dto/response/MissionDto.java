8af66e6e32509cb9d388b005ceac2a0ae96431a4
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-18 03:05:09 +0900

    fix: MissionDto에 각 유저별 userId, nickname, profileImageUrl 추가

--- src/main/java/com/beotkkotthon/areyousleeping/dto/response/MissionDto.java
+++ src/main/java/com/beotkkotthon/areyousleeping/dto/response/MissionDto.java
@@ -1,26 +1,37 @@
 package com.beotkkotthon.areyousleeping.dto.response;
 
 import com.beotkkotthon.areyousleeping.domain.Mission;
-import java.time.LocalDateTime;
+import com.beotkkotthon.areyousleeping.domain.User;
+import lombok.Builder;
 
+import java.time.LocalDateTime;
+@Builder
 public record MissionDto(
         Long id,
         Long userTeamId,
+        Long userId,
+        String nickname,
+        String profileImageUrl,
         String content,
         LocalDateTime issuedAt,
         Boolean result,
         String resultImageUrl,
         String resultText
 ) {
+
     public static MissionDto fromEntity(Mission mission) {
-        return new MissionDto(
-                mission.getId(),
-                mission.getUserTeam().getId(),
-                mission.getContent(),
-                mission.getIssuedAt(),
-                mission.getResult(),
-                mission.getResultImageUrl(),
-                mission.getResultText()
+        return MissionDto.builder()
+                .id(mission.getId())
+                .userTeamId(mission.getUserTeam().getId())
+                .userId(mission.getUserTeam().getUser().getId())
+                .nickname(mission.getUserTeam().getUser().getNickname())
+                .profileImageUrl(mission.getUserTeam().getUser().getProfileImageUrl())
+                .content(mission.getContent())
+                .issuedAt(mission.getIssuedAt())
+                .result(mission.getResult())
+                .resultImageUrl(mission.getResultImageUrl())
+                .resultText(mission.getResultText())
+                .build(
         );
     }
 }
