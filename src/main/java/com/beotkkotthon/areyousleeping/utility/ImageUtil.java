23aeeb4f090d5ef84ba3e5a8e3df65841d3a5fe6
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-18 02:11:06 +0900

    fix: ImageUtil에 uploadMissionImageFile 메소드 추가

--- src/main/java/com/beotkkotthon/areyousleeping/utility/ImageUtil.java
+++ src/main/java/com/beotkkotthon/areyousleeping/utility/ImageUtil.java
@@ -50,8 +50,7 @@ public class ImageUtil {
         return uuid + ".png";
     }
 
-
-    public String uploadImageFile(MultipartFile file, Long userId) {
+    public String uploadProfileImageFile(MultipartFile file, Long userId) {
         final String contentType = file.getContentType();
         assert contentType != null;
         String type = "." + contentType.substring(contentType.indexOf("/") + 1);
@@ -62,6 +61,27 @@ public class ImageUtil {
 
         String uuid = UUID.randomUUID().toString();
         String fileName = "user_" + userId + "/" + uuid + type;
+
+        try {
+            amazonS3Client.putObject(bucketName, fileName, file.getInputStream(), null);
+        } catch (IOException e) {
+            throw new CommonException(ErrorCode.UPLOAD_FILE_ERROR);
+        }
+
+        return bucketUrl + fileName;
+    }
+
+    public String uploadMissionImageFile(MultipartFile file, Long userId, Long teamId) {
+        final String contentType = file.getContentType();
+        assert contentType != null;
+        String type = "." + contentType.substring(contentType.indexOf("/") + 1);
+
+        if (!contentType.startsWith(IMAGE_CONTENT_PREFIX)) {
+            throw new CommonException(ErrorCode.MISSING_REQUEST_PARAMETER);
+        }
+
+        String uuid = UUID.randomUUID().toString();
+        String fileName = "user_" + userId + "team_" + teamId + "/" + uuid + type;
 
         try {
             amazonS3Client.putObject(bucketName, fileName, file.getInputStream(), null);
