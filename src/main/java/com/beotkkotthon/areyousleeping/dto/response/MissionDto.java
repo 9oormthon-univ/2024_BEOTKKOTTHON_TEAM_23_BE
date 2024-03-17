7b9ff2a99a61ff285433af7e2638d07e2a33208f
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-18 02:08:56 +0900

    feat: MissionDto 추가

--- src/main/java/com/beotkkotthon/areyousleeping/dto/response/MissionDto.java
+++ src/main/java/com/beotkkotthon/areyousleeping/dto/response/MissionDto.java
@@ -1,2 +1,26 @@
-package com.beotkkotthon.areyousleeping.dto.response;public class MissionDto {
+package com.beotkkotthon.areyousleeping.dto.response;
+
+import com.beotkkotthon.areyousleeping.domain.Mission;
+import java.time.LocalDateTime;
+
+public record MissionDto(
+        Long id,
+        Long userTeamId,
+        String content,
+        LocalDateTime issuedAt,
+        Boolean result,
+        String resultImageUrl,
+        String resultText
+) {
+    public static MissionDto fromEntity(Mission mission) {
+        return new MissionDto(
+                mission.getId(),
+                mission.getUserTeam().getId(),
+                mission.getContent(),
+                mission.getIssuedAt(),
+                mission.getResult(),
+                mission.getResultImageUrl(),
+                mission.getResultText()
+        );
+    }
 }
