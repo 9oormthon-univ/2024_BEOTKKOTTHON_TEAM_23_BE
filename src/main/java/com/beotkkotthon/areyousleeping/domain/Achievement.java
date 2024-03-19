d49888a82f92e9f13be85c116e3f41164ead9cca
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-20 01:27:10 +0900

    fix: AccessLevel Protected 추가

---
+++ src/main/java/com/beotkkotthon/areyousleeping/domain/Achievement.java
@@ -0,0 +1,45 @@
+package com.beotkkotthon.areyousleeping.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "achievement")
+public class Achievement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "achievement_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "difficulty", nullable = false)
+    private Integer difficulty;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Achievement(User user, String title, String content, Integer difficulty) {
+        this.user = user;
+        this.title = title;
+        this.content = content;
+        this.difficulty = difficulty;
+        this.createdAt = LocalDateTime.now();
+    }
+}
