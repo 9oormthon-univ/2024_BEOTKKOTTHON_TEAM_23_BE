50849f0094c47e21018c7952bfc1561d070417a6
leeeunda <daun0219@gmail.com>
2024-03-21 08:35:06 +0900

    feat : 밤샘메이트 조회 API

--- src/main/java/com/beotkkotthon/areyousleeping/service/UserTeamService.java
+++ src/main/java/com/beotkkotthon/areyousleeping/service/UserTeamService.java
@@ -1,8 +1,11 @@
 package com.beotkkotthon.areyousleeping.service;
 
+import com.beotkkotthon.areyousleeping.domain.Achievement;
 import com.beotkkotthon.areyousleeping.domain.Team;
 import com.beotkkotthon.areyousleeping.domain.User;
 import com.beotkkotthon.areyousleeping.domain.UserTeam;
+import com.beotkkotthon.areyousleeping.dto.response.TeamMemberInfoDto;
+import com.beotkkotthon.areyousleeping.repository.AchievementRepository;
 import com.beotkkotthon.areyousleeping.repository.TeamRepository;
 import com.beotkkotthon.areyousleeping.repository.UserRepository;
 import com.beotkkotthon.areyousleeping.repository.UserTeamRepository;
@@ -12,6 +15,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
@@ -21,6 +26,7 @@ public class UserTeamService {
     private final UserRepository userRepository;
     private final TeamRepository teamRepository;
     private final UserTeamRepository userTeamRepository;
+    private final AchievementRepository achievementRepository;
 
     // 밤샘 참여하기 버튼을 누른 사용자를 팀에 추가하는 메소드
     @Transactional
@@ -108,5 +114,25 @@ public class UserTeamService {
             throw new NoSuchElementException("해당 팀을 조회할 수 없습니다. " + teamId);
         }
         return userTeamRepository.countByTeamIdAndIsActiveTrue(teamId);
+    }
+
+    @Transactional
+    public List<TeamMemberInfoDto> getTeamMembersInfo(Long teamId){
+
+        // 해당 팀에 속한 모든 유저 조회
+        List<UserTeam> userTeams = userTeamRepository.findAllByTeamId(teamId);
+
+        List<TeamMemberInfoDto> teamMembersInfo = new ArrayList<>();
+        for (UserTeam userTeam : userTeams) {
+            User user = userTeam.getUser();
+            Achievement latestachievement = achievementRepository.findTopByUserIdOrderByAcquiredDateDesc(user.getId());
+
+            teamMembersInfo.add(TeamMemberInfoDto.builder()
+                    .userTeam(userTeam)
+                    .user(user)
+                    .achievement(latestachievement)
+                    .build());
+        }
+        return teamMembersInfo;
     }
 }
