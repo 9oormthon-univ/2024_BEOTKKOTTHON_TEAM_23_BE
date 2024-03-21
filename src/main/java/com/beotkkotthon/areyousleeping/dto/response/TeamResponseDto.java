7418e7b7a4b4942546e43cfe47ca0b1522f60179
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-21 15:23:01 +0900

    fix: TeamResponseDto record로 변경

--- src/main/java/com/beotkkotthon/areyousleeping/dto/response/TeamResponseDto.java
+++ src/main/java/com/beotkkotthon/areyousleeping/dto/response/TeamResponseDto.java
@@ -3,48 +3,34 @@ package com.beotkkotthon.areyousleeping.dto.response;
 import com.beotkkotthon.areyousleeping.domain.Team;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
-
-@Getter
-@Setter
+@Builder
 @Schema(description="팀 생성 응답 DTO")
-public class TeamResponseDto {
-
-    private Long id;
-    private String title;
-    private int maxNum;
-    private int currentNum;
-    private int targetTime;
-    private boolean isSecret;
-    private String password;
-    private String category;
-    private String description;
-    private List<Object> createdAt;
-
-    @Builder
-    public TeamResponseDto(Team team) {
-        this.id = team.getId();
-        this.title = team.getTitle();
-        this.maxNum = team.getMaxNum();
-        this.currentNum = team.getCurrentNum();
-        this.targetTime = team.getTargetTime();
-        this.isSecret = team.getIsSecret();
-        this.password = team.getPassword();
-        this.category = team.getCategory();
-        this.description = team.getDescription();
-        this.createdAt = Arrays.asList(
-                team.getCreatedAt().getYear(),
-                team.getCreatedAt().getMonthValue(),
-                team.getCreatedAt().getDayOfMonth(),
-                team.getCreatedAt().getHour(),
-                team.getCreatedAt().getMinute(),
-                team.getCreatedAt().getSecond(),
-                team.getCreatedAt().getNano()
-        );
+public record TeamResponseDto(
+        Long id,
+        String title,
+        int maxNum,
+        int currentNum,
+        int targetTime,
+        boolean isSecret,
+        String password,
+        String category,
+        String description,
+        LocalDateTime createdAt
+) {
+    public static TeamResponseDto fromEntity(Team team) {
+        return TeamResponseDto.builder()
+                .id(team.getId())
+                .title(team.getTitle())
+                .maxNum(team.getMaxNum())
+                .currentNum(team.getCurrentNum())
+                .targetTime(team.getTargetTime())
+                .isSecret(team.getIsSecret())
+                .password(team.getPassword())
+                .category(team.getCategory())
+                .description(team.getDescription())
+                .createdAt(team.getCreatedAt())
+                .build();
     }
 }
