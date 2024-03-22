35e0a4489346c9a74b342ec04013ae5f6dd51945
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-03-22 23:35:48 +0900

    fix: 로그인 로그 제거

--- src/main/java/com/beotkkotthon/areyousleeping/security/provider/JwtAuthenticationProvider.java
+++ src/main/java/com/beotkkotthon/areyousleeping/security/provider/JwtAuthenticationProvider.java
@@ -4,27 +4,22 @@ import com.beotkkotthon.areyousleeping.security.info.JwtUserInfo;
 import com.beotkkotthon.areyousleeping.security.info.UserPrincipal;
 import com.beotkkotthon.areyousleeping.security.service.CustomUserDetailService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationProvider implements AuthenticationProvider {
     private final CustomUserDetailService customUserDetailService;
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        log.info("AuthenticationProvider 진입 성공");
         if (authentication.getPrincipal().getClass().equals(String.class)){
             // form login 요청인 경우
-            log.info("로그인 로직 인증 과정");
             return authOfBeforeLogin(authentication.getPrincipal().toString());
 
         } else { // 로그인 이후의 경우에 String이 아닌 복합 정보가 들어오는 경우
-            log.info("로그인한 사용자 검증 과정");
             return authOfAfterLogin((JwtUserInfo) authentication.getPrincipal());
         }
     }
--- src/main/java/com/beotkkotthon/areyousleeping/security/service/CustomUserDetailService.java
+++ src/main/java/com/beotkkotthon/areyousleeping/security/service/CustomUserDetailService.java
@@ -5,11 +5,9 @@ import com.beotkkotthon.areyousleeping.exception.ErrorCode;
 import com.beotkkotthon.areyousleeping.repository.UserRepository;
 import com.beotkkotthon.areyousleeping.security.info.UserPrincipal;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailService implements UserDetailsService {
@@ -18,13 +16,11 @@ public class CustomUserDetailService implements UserDetailsService {
     public UserPrincipal loadUserByUsername(String username) throws UsernameNotFoundException {
         UserRepository.UserSecurityForm securityForm = userRepository.findSecurityFormBySerialId(username)
                 .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 아이디입니다."));
-        log.info("아이디 기반 조회 성공");
         return UserPrincipal.create(securityForm);
     }
     public UserPrincipal loadUserById(Long id) {
         UserRepository.UserSecurityForm userSecurityForm = userRepository.findSecurityFormById(id)
                 .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_USER));
-        log.info("user id 기반 조회 성공");
 
         return UserPrincipal.create(userSecurityForm);
     }
