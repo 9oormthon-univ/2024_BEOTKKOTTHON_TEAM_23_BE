3a0147fc70acad1ee56717beb3ba36b9eba6662f
leeeunda <daun0219@gmail.com>
2024-06-04 16:28:48 +0900

    fix: 유저 정보 조회 API 반환값 수정

--- src/main/java/com/beotkkotthon/areyousleeping/repository/UserTeamRepository.java
+++ src/main/java/com/beotkkotthon/areyousleeping/repository/UserTeamRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
@@ -23,4 +24,5 @@ public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
 
     List<UserTeam> findByTeamIdOrderByIdAsc(Long teamId);
 
+    Optional<UserTeam> findByUserId(Long userId);
 }
