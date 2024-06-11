5c044832a146ce367eb08e2ee770277ff4fffa84
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-06-11 17:02:27 +0900

    feat: 현재 팀 제외 마지막 UserTeam 찾는 Repository 메소드 구현

--- src/main/java/com/beotkkotthon/areyousleeping/repository/UserTeamRepository.java
+++ src/main/java/com/beotkkotthon/areyousleeping/repository/UserTeamRepository.java
@@ -4,6 +4,7 @@ import com.beotkkotthon.areyousleeping.domain.Team;
 import com.beotkkotthon.areyousleeping.domain.User;
 import com.beotkkotthon.areyousleeping.domain.UserTeam;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
 
     List<UserTeam> findAllByUserId(Long userId);
+    List<UserTeam> findAllByUserIdOrderByCreatedAtDesc(Long userId);
     List<UserTeam> findAllByTeamNotNull();
     UserTeam findByUserIdAndTeamId(Long userId, Long teamId);
     boolean existsByUserAndTeam(User user, Team team);
