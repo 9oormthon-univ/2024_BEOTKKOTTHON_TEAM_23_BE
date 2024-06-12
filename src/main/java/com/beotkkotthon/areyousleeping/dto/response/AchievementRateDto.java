420bdfd7042a6ff6a3f4d4155d69b17bdf722ec0
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-06-12 23:11:48 +0900

    fix: AchievementRateDto 추가

--- src/main/java/com/beotkkotthon/areyousleeping/dto/response/AchievementRateDto.java
+++ src/main/java/com/beotkkotthon/areyousleeping/dto/response/AchievementRateDto.java
@@ -1,2 +1,25 @@
-package com.beotkkotthon.areyousleeping.dto.response;public class AchievementRateDto {
+package com.beotkkotthon.areyousleeping.dto.response;
+
+import com.beotkkotthon.areyousleeping.domain.AchievementRate;
+import lombok.Builder;
+
+@Builder
+public record AchievementRateDto(
+        Integer allnightCount,
+        Long allnightTotal,
+        Integer idealAllnightCount,
+        Long leaderAllnightTotal,
+        Integer aloneAllnightCount,
+        Integer continuousAllnightCount
+) {
+    public static AchievementRateDto fromEntity(AchievementRate achievementRate) {
+        return AchievementRateDto.builder()
+                .allnightCount(achievementRate.getAllnightCount())
+                .allnightTotal(achievementRate.getAllnightTotal())
+                .idealAllnightCount(achievementRate.getIdealAllnightCount())
+                .leaderAllnightTotal(achievementRate.getLeaderAllnightTotal())
+                .aloneAllnightCount(achievementRate.getAloneAllnightCount())
+                .continuousAllnightCount(achievementRate.getContinuousAllnightCount())
+                .build();
+    }
 }
