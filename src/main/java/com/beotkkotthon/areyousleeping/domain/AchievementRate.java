3f25ffb6295969ddbf3988f88f4ed62b153b77e5
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-06-12 23:12:09 +0900

    fix: 달성률 업데이트 로직 도메인으로 이동

--- src/main/java/com/beotkkotthon/areyousleeping/domain/AchievementRate.java
+++ src/main/java/com/beotkkotthon/areyousleeping/domain/AchievementRate.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -56,5 +59,45 @@ public class AchievementRate {
         this.leaderAllnightTotal = leaderAllnightTotal;
         this.aloneAllnightCount = aloneAllnightCount;
         this.continuousAllnightCount = continuousAllnightCount;
+    }
+
+    public void updateAchievementRate(UserTeam userTeam, UserTeam lastUserTeam, Integer teamUsersCount) {
+
+        int addedAllnightCount = 0;
+        long addedAllnightTotal = 0L;
+        int addedIdealAllnightCount = 0;
+        long addedLeaderAllnightTotal = 0L;
+        int addedAloneAllnightCount = 0;
+        int addedContinuousAllnightCount = 0;
+
+        addedAllnightTotal += Duration.between(userTeam.getLastActiveAt(), LocalDateTime.now()).toMinutes();
+
+        if(Duration.between(lastUserTeam.getLastActiveAt(), userTeam.getLastActiveAt()).toHours() > 12) {
+            addedAllnightCount = 1;
+        }
+
+        if(userTeam.getIsLeader()) {
+            addedLeaderAllnightTotal += Duration.between(userTeam.getLastActiveAt(),LocalDateTime.now()).toMinutes();
+        }
+
+        if(Duration.between(userTeam.getLastActiveAt(),LocalDateTime.now()).toHours() > 6) {
+            addedIdealAllnightCount = 1;
+        }
+
+        if(teamUsersCount == 1) {
+            addedAloneAllnightCount = 1;
+        }
+
+        if(lastUserTeam.getIsActive() && userTeam.getIsActive() && Duration.between(lastUserTeam.getLastActiveAt(), userTeam.getLastActiveAt()).toHours() > 12) {
+            addedContinuousAllnightCount = 1;
+        }
+
+
+        this.allnightCount += addedAllnightCount;
+        this.allnightTotal += addedAllnightTotal;
+        this.idealAllnightCount += addedIdealAllnightCount;
+        this.leaderAllnightTotal += addedLeaderAllnightTotal;
+        this.aloneAllnightCount += addedAloneAllnightCount;
+        this.continuousAllnightCount += addedContinuousAllnightCount;
     }
 }
