4ff857a69be58581969d36f3ced5fcd965379e4f
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-07-16 23:38:58 +0900

    feat: 게시글 상세보기 api 구현

--- src/main/java/com/beotkkotthon/areyousleeping/dto/response/PostDetailResponseDto.java
+++ src/main/java/com/beotkkotthon/areyousleeping/dto/response/PostDetailResponseDto.java
@@ -1,26 +1,35 @@
 package com.beotkkotthon.areyousleeping.dto.response;
 
+import com.beotkkotthon.areyousleeping.domain.Comment;
 import com.beotkkotthon.areyousleeping.domain.Post;
 import com.beotkkotthon.areyousleeping.domain.User;
 import lombok.Builder;
 
+import java.util.List;
+
 @Builder
-public record PostResponseDto(
+public record PostDetailResponseDto(
         Long postId,
         String postTitle,
+        String postContent,
         Long userId,
         String nickname,
         String profileImageUrl,
-        String createdAt
+        String createdAt,
+        String updatedAt,
+        List<CommentResponseDto> commentsDto
 ) {
-    public static PostResponseDto fromEntity(Post post) {
-        return PostResponseDto.builder()
+    public static PostDetailResponseDto fromEntity(Post post, List<Comment> comments) {
+        return PostDetailResponseDto.builder()
                 .postId(post.getId())
                 .postTitle(post.getPostTitle())
+                .postContent(post.getPostContent())
                 .userId(post.getUser().getId())
                 .nickname(post.getUser().getNickname())
                 .profileImageUrl(post.getUser().getProfileImageUrl())
                 .createdAt(post.getCreatedAt().toString())
+                .updatedAt(post.getUpdatedAt().toString())
+                .commentsDto(CommentResponseDto.fromEntities(comments, List.of(post.getUser())))
                 .build();
     }
 }
